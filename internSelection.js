fb83139b68f41292d88c6c149e32254e98d12c0c
miz <miz@MIZZLE>
2024-04-10 13:18:12 +0000

    added javascript, login white bar swiping from the right side

---
+++ internSelection.js
@@ -0,0 +1,53 @@
+
+
+// login-selected
+
+function loginSelected(){
+    // loginForm
+    var loginForm = document.getElementsByClassName("login-form")[0];
+    loginForm.style.display = "flex";
+    loginForm.style.transition = 'opacity 2s ease';
+    setTimeout(() =>{
+        loginForm.style.opacity = 1;
+    }, 1);
+
+    // loginButton
+    var loginButton = document.getElementById("login-selected");
+    loginButton.style.display = "none";
+    loginButton.style.opacity = 0;
+
+    // signupButton
+    var signupButton = document.getElementById("signup-selected");
+    signupButton.style.display = "none";
+    signupButton.style.opacity = 0;
+
+    // login illustration
+    var rightDiv = document.querySelector('.login-selected-right-div');
+    rightDiv.style.transition = 'right 1s cubic-bezier(0.5, 1, 0.5, 1)';
+    rightDiv.style.right = '0vw';
+}
+
+function switchBack(){
+    var loginForm = document.getElementsByClassName("login-form")[0];
+    loginForm.style.transition = 'opacity 1s ease';
+    loginForm.style.opacity = 0;
+
+    var rightDiv = document.getElementsByClassName('login-selected-right-div')[0];
+    rightDiv.style.transition = 'right 1s cubic-bezier(0.5, 1, 0.5, 1)';
+    rightDiv.style.right = '-60vw';
+
+    loginForm.addEventListener('transitionend', function(){
+        loginForm.style.display = "none";
+        var loginButton = document.getElementById("login-selected");
+        var signupButton = document.getElementById("signup-selected");
+        loginButton.style.transition = 'opacity 2s ease';
+        signupButton.style.transition = 'opacity 2s ease';
+        loginButton.style.display = 'flex';
+        signupButton.style.display = 'flex';
+        setTimeout(() => {
+            loginButton.style.opacity = '1';
+            signupButton.style.opacity = '1';
+        }, 1);
+    })
+}
+
