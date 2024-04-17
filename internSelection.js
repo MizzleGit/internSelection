f74d2f5bd5c386641ee84727d22dee2728d6717a
Miz <sef.notirl@gmail.com>
2024-04-17 11:01:08 +0100

    wrote cleaner code for the signupselected and loginselected functions, fixing ids instead of classes in the commit after

--- internSelection.js
+++ internSelection.js
@@ -1,103 +1,126 @@
 const mediaMax750 = window.matchMedia("(max-width:705px)");
 
-// login-selected
-
+// If "Suivi votre demande" (Login) is selected
 function loginSelected(){
+
+
+    // Variables
+    let loginForm = document.getElementById("login-form");
+    let loginButton = document.getElementById("login-selected");
+    let signupButton = document.getElementById("signup-selected");
+    let rightDiv = document.querySelector('.login-selected-right-div');
+
+
+    // For mobile (Work in progress)
     if (mediaMax750.matches){
-        var loginForm = document.getElementsByClassName("login-form")[0];
         document.getElementsByClassName("signup-right")[0].style.display = "none";
         document.getElementsByClassName("login-left")[0].style.height = "100vh";
         document.getElementsByClassName("custom-button")[0].style.display = "none";
         loginForm.style.display = "flex";
         loginForm.style.opacity = 1;
     }
+
+
+    // For desktop
     else{
-        document.getElementsByClassName("login-selected-right-div")[0].style.opacity = "1";
-        // loginForm
-        var loginForm = document.getElementsByClassName("login-form")[0];
+        // Making login illustration appear from the right
+        rightDiv.style.opacity = "1";
+        rightDiv.style.transition = 'right 1s cubic-bezier(0.5, 1, 0.5, 1)';
+        rightDiv.style.right = '0vw';
+
+        // Making "Suivi votre demande" button disappear
+        loginButton.style.display = "none";
+        loginButton.style.opacity = 0;
+
+        // Making "S'inscrire pour un stage" button disappear
+        signupButton.style.display = "none";
+        signupButton.style.opacity = 0;
+
+        // Making Login form appear with opacity
         loginForm.style.display = "flex";
         loginForm.style.transition = 'opacity 1s ease';
         setTimeout(() =>{
             loginForm.style.opacity = 1;
         }, 1);
 
-        // loginButton
-        var loginButton = document.getElementById("login-selected");
-        loginButton.style.display = "none";
-        loginButton.style.opacity = 0;
 
-        // signupButton
-        var signupButton = document.getElementById("signup-selected");
-        signupButton.style.display = "none";
-        signupButton.style.opacity = 0;
-
-        // login illustration
-        var rightDiv = document.querySelector('.login-selected-right-div');
-        rightDiv.style.transition = 'right 1s cubic-bezier(0.5, 1, 0.5, 1)';
-        rightDiv.style.right = '0vw';
-    }
-}
+    } // End of else
+} // End of function loginSelected
 
 function signupSelected(){
-    document.getElementsByClassName("signup-selected-left-div")[0].style.opacity = "1";
-    var signupForm = document.getElementsByClassName("signup-form")[0];
+
+
+    // Variables
+    let leftDiv = document.getElementsByClassName("signup-selected-left-div")[0];
+    let signupForm = document.getElementsByClassName("signup-form")[0];
+    let signupButton = document.getElementById("signup-selected");
+    let loginButton = document.getElementById("login-selected");
+
+
+    // Making signup illustration appear from the left
+    leftDiv.style.opacity = "1";
+    leftDiv.style.transition = 'left 1s cubic-bezier(0.5, 1, 0.5, 1)';
+    leftDiv.style.left = '0vw';    
+
+    // Making "S'inscrire pour un stage" button disappear
+    signupButton.style.display = "none";
+    signupButton.style.opacity = 0;
+
+    // Making "Suivi votre demande" button disappear
+    loginButton.style.display = "none";
+    loginButton.style.opacity = 0;
+
+    // Making Signup form appear with opacity
     signupForm.style.display = "flex";
     signupForm.style.transition = 'opacity 1s ease';
     setTimeout(() =>{
         signupForm.style.opacity = 1;
     }, 1);
-
-    // loginButton
-    var signupButton = document.getElementById("signup-selected");
-    signupButton.style.display = "none";
-    signupButton.style.opacity = 0;
-
-    // signupButton
-    var loginButton = document.getElementById("login-selected");
-    loginButton.style.display = "none";
-    loginButton.style.opacity = 0;
-
-    // login illustration
-    var leftDiv = document.querySelector('.signup-selected-left-div');
-    leftDiv.style.transition = 'left 1s cubic-bezier(0.5, 1, 0.5, 1)';
-    leftDiv.style.left = '0vw';
-}
+} // End of function signupSelected
 
 function switchBack(){
-    var loginForm = document.getElementsByClassName("login-form")[0];
+    let loginForm = document.getElementById("login-form");
+    let signupForm = document.getElementsByClassName("signup-form")[0];
+    let rightDiv = document.getElementsByClassName('login-selected-right-div')[0];
+    let leftDiv = document.getElementsByClassName('signup-selected-left-div')[0];
+    let loginButton = document.getElementById("login-selected");
+    let signupButton = document.getElementById("signup-selected");
+
+    // Forms opacity 0 - Divs move away
+    //
     loginForm.style.transition = 'opacity 0.5s ease';
     loginForm.style.opacity = 0;
-
     // 
-    var signupForm = document.getElementsByClassName("signup-form")[0];
     signupForm.style.transition = 'opacity 0.5s ease';
     signupForm.style.opacity = 0;
     // 
-
-    var rightDiv = document.getElementsByClassName('login-selected-right-div')[0];
     rightDiv.style.transition = 'right 1s cubic-bezier(0.5, 1, 0.5, 1)';
     rightDiv.style.right = '-60vw';
-
     // 
-    var leftDiv = document.getElementsByClassName('signup-selected-left-div')[0];
     leftDiv.style.transition = 'left 1s cubic-bezier(0.5, 1, 0.5, 1)';
     leftDiv.style.left = '-60vw';
     // 
 
+    // Forms display = none | buttons appear | divs get opacity 0
     setTimeout(() => {
+
         loginForm.style.display = "none";
         signupForm.style.display = "none";
-        var loginButton = document.getElementById("login-selected");
-        var signupButton = document.getElementById("signup-selected");
+
         loginButton.style.transition = 'opacity 0.5s ease';
         signupButton.style.transition = 'opacity 0.5s ease';
+
         loginButton.style.display = 'flex';
         signupButton.style.display = 'flex';
+        
         setTimeout(() => {
+
             loginButton.style.opacity = '1';
             signupButton.style.opacity = '1';
-            document.getElementsByClassName("signup-selected-left-div")[0].style.opacity = "0";
-            document.getElementsByClassName("login-selected-right-div")[0].style.opacity = "0";
+
+            leftDiv.style.opacity = "0";
+            rightDiv.style.opacity = "0";
+
         }, 500);
     }, 500);
 
